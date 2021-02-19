aac664e7b3961ae47d695159a0c96c1a1d74ae77
张宇亮 <zhangyuliang@blued.com>
2021-02-19 18:22:37 +0800

    first

---
+++ app/src/main/java/com/wonbin/autoclick/FloatingView.java
@@ -0,0 +1,118 @@
+package com.wonbin.autoclick;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class FloatingView extends FrameLayout implements View.OnClickListener {
+    private Context mContext;
+    private View mView;
+    private ImageView mPlayView;
+    private ImageView mStopView;
+    private ImageView mCloseView;
+    private int mTouchStartX, mTouchStartY;//手指按下时坐标
+    private WindowManager.LayoutParams mParams;
+    private FloatingManager mWindowManager;
+    public String mCurState;
+
+    public FloatingView(Context context) {
+        super(context);
+        mContext = context.getApplicationContext();
+        LayoutInflater mLayoutInflater = LayoutInflater.from(context);
+        mView = mLayoutInflater.inflate(R.layout.floating_view, null);
+
+        mPlayView = (ImageView) mView.findViewById(R.id.play);
+        mStopView = (ImageView) mView.findViewById(R.id.stop);
+        mCloseView = (ImageView) mView.findViewById(R.id.close);
+        mPlayView.setOnClickListener(this);
+        mStopView.setOnClickListener(this);
+        mCloseView.setOnClickListener(this);
+
+        mWindowManager = FloatingManager.getInstance(mContext);
+    }
+
+    public void show() {
+        mParams = new WindowManager.LayoutParams();
+        mParams.gravity = Gravity.CENTER;
+        //总是出现在应用程序窗口之上
+        if (Build.VERSION.SDK_INT >= 26) {
+            mParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            mParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        }
+        //设置图片格式，效果为背景透明
+        mParams.format = PixelFormat.RGBA_8888;
+        mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR |
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+        mParams.width = LayoutParams.WRAP_CONTENT;
+        mParams.height = LayoutParams.WRAP_CONTENT;
+        boolean result = mWindowManager.addView(mView, mParams);
+        Toast.makeText(getContext(), "浮窗显示:" + result, Toast.LENGTH_LONG).show();
+        mView.setOnTouchListener(mOnTouchListener);
+    }
+
+    public void hide() {
+        mWindowManager.removeView(mView);
+    }
+
+    private OnTouchListener mOnTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    mTouchStartX = (int) event.getRawX();
+                    mTouchStartY = (int) event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (!AutoService.PLAY.equals(mCurState)) {
+                        mParams.x += (int) event.getRawX() - mTouchStartX;
+                        mParams.y += (int) event.getRawY() - mTouchStartY;//相对于屏幕左上角的位置
+                        mWindowManager.updateView(mView, mParams);
+                        mTouchStartX = (int) event.getRawX();
+                        mTouchStartY = (int) event.getRawY();
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getContext(), AutoService.class);
+        switch (view.getId()) {
+            case R.id.play:
+                mCurState = AutoService.PLAY;
+                int[] location = new int[2];
+                mView.getLocationOnScreen(location);
+                intent.putExtra(AutoService.ACTION, AutoService.PLAY);
+                intent.putExtra("x", location[0] - 1);
+                intent.putExtra("y", location[1] - 1);
+                break;
+            case R.id.stop:
+                mCurState = AutoService.STOP;
+                intent.putExtra(AutoService.ACTION, AutoService.STOP);
+                break;
+            case R.id.close:
+                intent.putExtra(AutoService.ACTION, AutoService.HIDE);
+                Intent appMain = new Intent(getContext(), MainActivity.class);
+                getContext().startActivity(appMain);
+                break;
+        }
+        getContext().startService(intent);
+    }
+}
---
+++ app/src/main/java/com/wonbin/autoclick/MainActivity.java
@@ -0,0 +1,73 @@
+package com.wonbin.autoclick;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mStart;
+    TextView per1, per2;
+    private EditText mInterval;
+    private RadioGroup mCheckMode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mStart = findViewById(R.id.start);
+        per1 = findViewById(R.id.permission_1);
+        per2 = findViewById(R.id.permission_2);
+        mInterval = findViewById(R.id.interval);
+        mCheckMode = findViewById(R.id.check_mode);
+        mStart.setOnClickListener(this);
+        per1.setOnClickListener(this);
+        per2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, AutoService.class);
+        switch (v.getId()) {
+            case R.id.start:
+                intent.putExtra(AutoService.ACTION, AutoService.SHOW);
+                intent.putExtra("interval", Integer.valueOf(mInterval.getText().toString()));
+                int id = mCheckMode.getCheckedRadioButtonId();
+                intent.putExtra(AutoService.MODE, id == R.id.swipe ? AutoService.SWIPE : AutoService.TAP);
+                startService(intent);
+                finish();
+                break;
+            case R.id.permission_1:
+                Intent intent2 = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                startActivity(intent2);
+                break;
+            case R.id.permission_2:
+                Intent intentFloat = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(intentFloat);
+                break;
+        }
+    }
+
+
+}
